0eed3a9f05995095253d92d5b09396e6464389ac
Gary Lu <gary.lu@uct.ac.za>
2022-09-16 12:52:57 +0200

    Refactor APIs
    Refactor APIs and add jstor id parameter queries
    Add new tests
    
    Testing:
    python manage.py test api --keepdb

--- api/urls.py
+++ api/urls.py
@@ -17,26 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(
-        "api/articles/metadata",
-        views.store_metadata,
-        name="store_metadata",
-    ),
-    path("api/articles", views.get_all_articles, name="get_all_articles"),
-    path("api/articles/title", views.get_article_by_title, name="get_article_by_title"),
-    path(
-        "api/articles/author",
-        views.get_articles_by_author,
-        name="get_articles_by_author",
-    ),
-    path(
-        "api/articles/journal",
-        views.get_articles_from_journal,
-        name="get_articles_from_journal",
-    ),
-    path(
-        "api/journals/titles",
-        views.get_all_journals,
-        name="get_all_journals",
-    ),
+    path("api/articles", views.get_articles, name="get_articles"),
+    path("api/journals", views.get_journals, name="get_journals"),
 ]
