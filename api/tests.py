14f1fd71c3e941d03ebb0fab5e54a11c3f109f31
Gary Lu <gary.lu@uct.ac.za>
2022-09-16 12:52:57 +0200

    Refactor APIs
    Refactor APIs and add jstor id parameter queries
    Add new tests
    
    Testing:
    python manage.py test api --keepdb

--- api/tests.py
+++ api/tests.py
@@ -1,6 +1,3 @@
-import json
-import os
-
 from django.core.management import call_command
 from django.test import TestCase, Client
 from django.urls import reverse
@@ -8,10 +5,9 @@ from rest_framework import status
 
 from api.models import (
     Journal,
-    Issue,
     Article,
-    Author,
 )
+from api.views import ONLY_JSTOR_ID
 
 client = Client()
 
@@ -20,8 +16,8 @@ class TestArticle(TestCase):
     def setUp(self):
         call_command("loaddata", "fixtures/test_fixtures", verbosity=0)
 
-    def test_get_all_articles(self):
-        response = client.get(reverse("get_all_articles"))
+    def test_get_articles(self):
+        response = client.get(reverse("get_articles"))
 
         articles = Article.objects.all()
 
@@ -33,83 +29,97 @@ class TestArticle(TestCase):
         title = "The Larval Inhabitants of Cow Pats"
 
         response = client.get(
-            "%s?title=%s" % (reverse("get_article_by_title"), incorrect_title)
+            "%s?title=%s" % (reverse("get_articles"), incorrect_title)
         ).data[0]
 
         article = Article.objects.select_related("issue").get(title=title)
 
         self.assertEqual(response["articleID"], article.articleID)
 
+    def test_get_article_jstor_ids_by_title(self):
+        incorrect_title = "The Inhabitanst of Cat Pats"
+        title = "The Larval Inhabitants of Cow Pats"
+
+        response = client.get(
+            "%s?title=%s&%s=1"
+            % (reverse("get_articles"), incorrect_title, ONLY_JSTOR_ID),
+        ).data[0]
+
+        article = Article.objects.select_related("issue").get(title=title)
+
+        self.assertEqual(response["articleJstorID"], article.articleJstorID)
+        self.assertEqual(response.get("articleID"), None)
+
     def test_get_articles_by_author(self):
         incorrect_author_name = "Lawrence"
 
         response = client.get(
-            "%s?authorName=%s"
-            % (reverse("get_articles_by_author"), incorrect_author_name)
+            "%s?authorName=%s" % (reverse("get_articles"), incorrect_author_name)
         ).data[0]
 
         article = Article.objects.get(title="The Larval Inhabitants of Cow Pats")
 
         self.assertEqual(response["articleID"], article.articleID)
 
+    def test_get_article_jstor_ids_by_author(self):
+        incorrect_author_name = "Lawrence"
+
+        response = client.get(
+            "%s?authorName=%s&%s=1"
+            % (reverse("get_articles"), incorrect_author_name, ONLY_JSTOR_ID)
+        ).data[0]
+
+        article = Article.objects.get(title="The Larval Inhabitants of Cow Pats")
+
+        self.assertEqual(response["articleJstorID"], article.articleJstorID)
+        self.assertEqual(response.get("articleID"), None)
+
     def test_get_articles_from_journal(self):
         incorrect_journal_name = "Amimal Ecology"
 
         response = client.get(
-            "%s?journalName=%s"
-            % (reverse("get_articles_from_journal"), incorrect_journal_name)
+            "%s?journalName=%s" % (reverse("get_articles"), incorrect_journal_name)
         )
 
         articles = Article.objects.all()
 
         self.assertEqual(len(response.data), len(articles))
 
+    def test_get_article_jstor_ids_from_journal(self):
+        incorrect_journal_name = "Amimal Ecology"
 
-class TestMetadata(TestCase):
-    def test_upload_metadata(self):
-        headers = {"Content-Type": "application/json", "Accept": "application/json"}
-
-        with open("fixtures/test_metadata_small.json") as f:
-            metadata = json.load(f)
-
-        response = client.post(
-            "%s?apiKey=%s"
-            % (reverse("store_metadata"), os.environ.get("METADATA_API_KEY")),
-            data={"metadata": json.dumps(metadata)},
-            headers=headers,
+        response = client.get(
+            "%s?journalName=%s&%s=1"
+            % (reverse("get_articles"), incorrect_journal_name, ONLY_JSTOR_ID)
         )
 
-        # Test journal
-        journal_response = Journal.objects.get(
-            journalName="14th Century English Mystics Newsletter"
-        )
-        self.assertEqual(journal_response.issn, "07375840")
-        self.assertEqual(journal_response.altISSN, "")
+        articles = Article.objects.all()
 
-        # Test issue
-        issue_response = Issue.objects.get(issueJstorID="1")
-        self.assertEqual(issue_response.journal, journal_response)
-        self.assertEqual(issue_response.volume, 9)
-        self.assertEqual(issue_response.number, 4)
-        self.assertEqual(issue_response.year, 1983)
+        self.assertEqual(len(response.data), len(articles))
+        self.assertEqual(response.data[0].get("articleID"), None)
 
-        # Test articles
-        article_response_1 = Article.objects.get(articleJstorID="1")
-        self.assertEqual(article_response_1.issue, issue_response)
-        self.assertEqual(article_response_1.title, "Front Matter")
-        self.assertEqual(article_response_1.abstract, "")
-        self.assertEqual(article_response_1.bucketURL, None)
-        article_response_2 = Article.objects.get(articleJstorID="2")
-        self.assertEqual(article_response_2.issue, issue_response)
-        self.assertEqual(article_response_2.title, "TO OUR READERS")
-        self.assertEqual(article_response_2.abstract, "")
-        self.assertEqual(article_response_2.bucketURL, None)
 
-        # Test authors
-        author_1 = Author.objects.get(authorName="blah")
-        author_2 = Author.objects.get(authorName="blah 2")
+class TestJournal(TestCase):
+    def setUp(self):
+        call_command("loaddata", "fixtures/test_fixtures", verbosity=0)
 
-        self.assertEqual(len(author_1.article_set.all()), 2)
-        self.assertEqual(len(author_2.article_set.all()), 1)
+    def test_get_journals(self):
+        response = client.get(reverse("get_journals"))
 
+        journals = Journal.objects.all()
+
+        self.assertEqual(len(response.data), len(journals))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_journals_by_name(self):
+
+        incorrect_journal_name = "Animimal Ecology"
+        journal_name = "Journal of Animal Ecology"
+
+        response = client.get(
+            "%s?journalName=%s" % (reverse("get_journals"), incorrect_journal_name)
+        ).data[0]
+
+        journal = Journal.objects.get(journalName=journal_name)
+
+        self.assertEqual(response["journalID"], journal.journalID)
