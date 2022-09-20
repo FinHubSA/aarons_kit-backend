02d6806754ff1fe820f5083da04cc623ef322506
takundachirema <takundachirema@Takundas-MBP.home>
2022-09-20 16:32:07 +0200

    tests

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.contrib.postgres.search import TrigramSimilarity
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
